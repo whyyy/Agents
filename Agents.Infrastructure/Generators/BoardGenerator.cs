bbc0bd1249f3d1159e9300d360f2b56fe92d9f71
binas <matib95@gmail.com>
2020-10-25 18:54:37 +0100

    fix: Creating board with 25 items only + txt coding update

--- Agents.Infrastructure/Generators/BoardGenerator.cs
+++ Agents.Infrastructure/Generators/BoardGenerator.cs
@@ -1,5 +1,7 @@
-﻿using Agents.Infrastructure.Helpers;
+﻿using System;
+using Agents.Infrastructure.Helpers;
 using Agents.Models;
+using Agents.Models.Helpers;
 
 namespace Agents.Infrastructure.Generators
 {
@@ -7,11 +9,13 @@ namespace Agents.Infrastructure.Generators
     {
         private readonly IWordsLoader _wordsLoader;
         private readonly IBoard _board;
+        private readonly Random _random;
 
         public BoardGenerator(IWordsLoader wordsLoader, IBoard board)
         {
             _wordsLoader = wordsLoader;
             _board = board;
+            _random = new Random();
         }
         
         public void GenerateBoard(LoadingType loadingType)
@@ -23,11 +27,20 @@ namespace Agents.Infrastructure.Generators
             }
 
             var position = 1;
-            foreach (var word in _wordsLoader.StringWords)
+
+            while (_board.Words.Count < KeyCardRule.KeyCardSize)
             {
+                var word = PickRandomWord();
                 _board.Words.Add(new Word(word, position));
+                _wordsLoader.StringWords.Remove(word);
                 position++;
             }
+        }
+
+        private string PickRandomWord()
+        {
+            var index = _random.Next(_wordsLoader.StringWords.Count);
+            return _wordsLoader.StringWords[index];
         }
     }
 }
