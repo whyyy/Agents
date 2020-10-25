c6fb1de05b63cf4881d88006ef852d68a145160b
binas <matib95@gmail.com>
2020-10-25 15:30:56 +0100

    feat: base logic for Word class

---
+++ Agents.Models/IWord.cs
@@ -0,0 +1,12 @@
+﻿using Agents.Models.Helpers;
+
+namespace Agents.Models
+{
+    public interface IWord
+    {
+        public int Position { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public ColorName ColorName { get; set; }
+    }
+}
