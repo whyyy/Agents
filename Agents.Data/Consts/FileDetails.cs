b5cc10350cfc391a36668584a9bc320eb740731f
binas <matib95@gmail.com>
2020-10-25 17:33:17 +0100

    feat: Adding consts for file

---
+++ Agents.Data/Consts/FileDetails.cs
@@ -0,0 +1,7 @@
+﻿namespace Agents.Data.Consts
+{
+    public static class FileDetails
+    {
+        public const string FilePath = @"D:\repos\AgentsGame\Agents\Agents\Agents.Data\Words.txt";
+    }
+}
